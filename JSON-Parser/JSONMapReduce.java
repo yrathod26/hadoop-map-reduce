c5e9c813bce884aaf6c0582c8db62ab8d3e7d55b
Yash Rathod <55633528+yrathod26@users.noreply.github.com>
2019-10-10 00:45:13 -0400

    Add files via upload

---
+++ JSON-Parser/JSONMapReduce.java
@@ -0,0 +1,67 @@
+package com.example.q4;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+import java.io.File;
+import java.io.IOException;
+
+public class JSONMapReduce {
+
+
+    public static void main(String[] args) throws Exception {
+            Configuration conf = new Configuration();
+            String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+            if (otherArgs.length != 2) {
+                System.err.println("Usage: jsonMapReduce <in> <out>");
+            System.exit(2);
+        }
+
+        @SuppressWarnings("deprecation")
+        Job job = new Job(conf, "jsonMapReduce");
+        job.setJarByClass(JSONMapReduce.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+//        job.setNumReduceTasks(0);
+        job.setInputFormatClass(JSONInputFormat.class);
+        job.setOutputKeyClass(LongWritable.class);
+        job.setOutputValueClass(Text.class);
+        FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+        File file = new File(otherArgs[1]);
+        FileUtils.deleteDirectory(file);
+        FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+
+    public static class TokenizerMapper extends Mapper<Object, Text, LongWritable, Text> {
+
+        private Text word = new Text();
+
+        public void map(Object key, Text value, Context context
+        ) throws IOException, InterruptedException {
+                context.write((LongWritable) key, value);
+        }
+    }
+
+    public static class IntSumReducer extends Reducer<LongWritable, Text, LongWritable, Text> {
+
+        public void reduce(LongWritable key, Iterable<Text> values,
+                           Context context
+        ) throws IOException, InterruptedException {
+            for (Text val : values) {
+                context.write(key, val);
+            }
+        }
+    }
+
+}
